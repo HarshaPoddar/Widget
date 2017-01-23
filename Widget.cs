5ece28e21bce73b794358be33698c5ccb319ceb8
Harshvardhan Poddar <harsha.poddar@yahoo.com>
2017-01-23 21:50:51 +0530

    Changed char DataType to String to Validate all conditions!

--- Widget.cs
+++ Widget.cs
@@ -12,12 +12,12 @@ namespace Drawing
         static void Main(string[] args)
         {
             Console.Write("Do you want to add a widget?(Y/N) :- ");
-            char add = Convert.ToChar(Console.ReadLine());
-            char Temporary = 'Y';
-            while (Temporary == 'Y' || Temporary == 'y')
+            string add = Console.ReadLine();
+            string Temporary = "Y";
+            while (Temporary == "Y" || Temporary == "y")
             {
                 Console.Clear();
-                if (add == 'Y' || add == 'y')
+                if (add == "Y" || add == "y")
                 {
                     Console.WriteLine("Which widget do you want to add?");
                     Console.WriteLine("(Answer just the number corresponding to the widget) ");
@@ -27,50 +27,50 @@ namespace Drawing
                     Console.WriteLine("4.Textbox ");
                     Console.WriteLine("5.Exit");
                     Console.WriteLine("");
-                    char Answer = Convert.ToChar(Console.ReadLine());
-                    if (Answer == '1')
+                    string Answer = Console.ReadLine();
+                    if (Answer == "1")
                     {
                         Rectangle obj = new Drawing.Rectangle();
                         obj.Main();
                     }
-                    else if (Answer == '2')
+                    else if (Answer == "2")
                     {
                         Square obj = new Drawing.Square();
                         obj.Main();
                     }
-                    else if (Answer == '3')
+                    else if (Answer == "3")
                     {
                         Circle obj = new Drawing.Circle();
                         obj.Main();
 
                     }
-                    else if (Answer == '4')
+                    else if (Answer == "4")
                     {
                         Textbox obj = new Textbox();
                         obj.Main();
 
                     }
-                    else if (Answer == '5')
+                    else if (Answer == "5")
                     {
-                        add = 'N';
+                        add = "N";
                         continue;
                     }
                     else
                     {
                         Console.WriteLine("Wrong Entry! Try Again?");
                         Console.WriteLine("Do you want to add a widget?(Y/N) :-");
-                        add = Convert.ToChar(Console.ReadLine());
+                        add = Console.ReadLine();
                         continue;
                     }
                     Console.Write("Do you want to add another widget?(Y/N) :- ");
-                    add = Convert.ToChar(Console.ReadLine());
+                    add = Console.ReadLine();
                     continue;
                 }
 
-                else if (add == 'N' || add == 'n')
+                else if (add == "N" || add == "n")
                 {
                     Console.WriteLine("Alright!");
-                    add = 'N';
+                    add = "N";
                     break;
                 }
                 else
@@ -79,7 +79,7 @@ namespace Drawing
 
                     Console.WriteLine("Wrong entry! Please enter again!");
                     Console.Write("Do you want to add another widget?(Y/N) :- ");
-                    add = Convert.ToChar(Console.ReadLine());
+                    add = Console.ReadLine();
                     continue;
                 }
 
